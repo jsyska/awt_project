5c91dde2ea3d903dd57534fa7005509cf380f762
Jakub Syska <jsyska@it-dev.pl>
2023-03-23 14:13:11 +0100

    Refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,8 @@ import path from "path";
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the "client/dist" directory
 app.use(express.static(path.join(__dirname, "..", ".." , "client", "dist")));
-console.log(__dirname)
-// Serve the index.html file from the "client/dist" directory
+
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "..", "..", "client", "dist", "index.html"));
 });
