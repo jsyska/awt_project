d6fc999bc311553929ab64be77414a37df4b2a63
Jakub Syska <jsyska@it-dev.pl>
2023-03-23 13:50:47 +0100

    Fixed server config

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import path from "path";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the "client/dist" directory
+app.use(express.static(path.join(__dirname, "..", ".." , "client", "dist")));
+console.log(__dirname)
+// Serve the index.html file from the "client/dist" directory
 app.get("/", (req: Request, res: Response) => {
-  res.send("NETIZEN SPACE");
+  res.sendFile(path.join(__dirname, "..", "..", "client", "dist", "index.html"));
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}/ `);
+  console.log(`Server is running on http://localhost:${port}/`);
 });
