2e6dc77ce066a25c9f7845f06a21ebf23e66827d
Jakub Syska <jsyska@it-dev.pl>
2023-03-29 01:49:01 +0200

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,9 +29,9 @@ app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "../public/assets")));
-app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
+app.use(express.static(path.join(__dirname, "..", "..", "..", "client", "dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..","..","client", "dist",     
+  res.sendFile(path.resolve(__dirname, "..", "..", "..","client", "dist",     
   "index.html"));
 });
 
