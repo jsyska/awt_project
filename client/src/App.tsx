c3b777ef8ce80e57f170a8e431f8100a1625cbeb
Jan Krokos <jankrokos@gmail.com>
2023-03-28 21:45:38 +0200

    Add dark mode and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,23 @@ import LoginPage from "./pages/login";
 import RegisterPage from "./pages/register";
 import ChatPage from "./pages/chat";
 import ProfilePage from "./pages/profile";
+import Navbar from "./components/navbar";
 
 function App() {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { useSelector } from "react-redux/es/exports";
+import { useNavigate } from "react-router-dom";
+import { AuthState } from "../redux";
+import {
+  BellIcon,
+  ChatBubbleBottomCenterTextIcon,
+  MagnifyingGlassIcon,
+  SunIcon,
+} from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navigate = useNavigate();
+  const user = useSelector((state: AuthState) => state.user);
+  console.log(user);
+
+  return (
+    <>
+      {true ? (
+        <div className="flex items-center justify-between bg-slate-400 py-4 px-8 dark:bg-slate-800">
+          <div className="flex items-center justify-between gap-3">
+            <span
+              className="cursor-pointer text-3xl font-bold text-slate-200 hover:text-slate-300"
+              onClick={() => navigate("/")}
+            >
+              NETIZEN SPACE
+            </span>
+            <div className="flex items-center justify-between gap-4 rounded-lg px-6 py-1 max-md:hidden">
+              <input
+                type="text"
+                placeholder="Search"
+                className="rounded-md py-2 px-4 outline-none dark:bg-slate-900"
+              />
+              <MagnifyingGlassIcon className="h-6 w-6 cursor-pointer dark:text-white" />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between gap-7">
+            <SunIcon
+              className="h-6 w-6 cursor-pointer dark:text-white max-sm:hidden"
+              onClick={() => {
+                if (document.documentElement.classList.contains("dark")) {
+                  document.documentElement.classList.remove("dark");
+                  localStorage.theme = "light";
+                } else {
+                  document.documentElement.classList.add("dark");
+                  localStorage.theme = "dark";
+                }
+              }}
+            />
+            <ChatBubbleBottomCenterTextIcon className="h-6 w-6 cursor-pointer dark:text-white max-sm:hidden" />
+            <BellIcon className="h-6 w-6 cursor-pointer dark:text-white max-sm:hidden" />
+            <div>
+              <img
+                src="#"
+                alt="Profile pic"
+                data-dropdown-toggle="dropdown"
+                className="h-12 w-12 cursor-pointer rounded-full bg-slate-500"
+                onClick={() => navigate(`/profile/${user?._id}`)}
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center bg-slate-400 py-4 px-8 dark:bg-slate-800">
+          <span className="text-3xl font-bold text-slate-200">
+            NETIZEN SPACE
+          </span>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Navbar;
