9516db7734a1e0d5594eb1d7ed7dbec6633b8eff
Jan Krokos <jankrokos@gmail.com>
2023-03-29 12:00:02 +0200

    Add logout to navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,28 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux/es/exports";
-import { useNavigate } from "react-router-dom";
-import { AuthState } from "../redux";
+import { useDispatch, useSelector } from "react-redux/es/exports";
+import { Link } from "react-router-dom";
+import { AuthState, setLogout } from "../redux";
 import {
   BellIcon,
   ChatBubbleBottomCenterTextIcon,
   MagnifyingGlassIcon,
   SunIcon,
 } from "@heroicons/react/24/solid";
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 const Navbar = () => {
-  const navigate = useNavigate();
   const user = useSelector((state: AuthState) => state.user);
-  console.log(user);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {};
 
   return (
     <div className="flex items-center justify-between bg-slate-400 py-4 px-8 dark:bg-slate-800">
       <div className="flex items-center justify-between gap-3">
-        <span
-          className="cursor-pointer text-3xl font-bold text-slate-200 hover:text-slate-300"
-          onClick={() => navigate("/")}
-        >
-          NETIZEN SPACE
-        </span>
+        <Link to="/">
+          <span className="cursor-pointer text-3xl font-bold text-slate-200 hover:text-slate-300">
+            NETIZEN SPACE
+          </span>
+        </Link>
         <div className="flex items-center justify-between gap-4 rounded-lg px-6 py-1 max-md:hidden">
           <input
             type="text"
@@ -46,20 +49,62 @@ const Navbar = () => {
             }
           }}
         />
-        <ChatBubbleBottomCenterTextIcon
-          className="h-6 w-6 cursor-pointer dark:text-white max-sm:hidden"
-          onClick={() => navigate(`/chat`)}
-        />
+        <Link to="/chat">
+          <ChatBubbleBottomCenterTextIcon className="h-6 w-6 cursor-pointer dark:text-white max-sm:hidden" />
+        </Link>
         <BellIcon className="h-6 w-6 cursor-pointer dark:text-white max-sm:hidden" />
-        <div>
-          <img
-            src="#"
-            alt="Profile pic"
-            data-dropdown-toggle="dropdown"
-            className="h-12 w-12 cursor-pointer rounded-full bg-slate-500"
-            onClick={() => navigate(`/profile/${user?._id}`)}
-          />
-        </div>
+
+        <Menu as="div" className="relative inline-block">
+          <div>
+            <Menu.Button className="inline-flex w-full justify-center rounded-md bg-slate-100 px-4 py-2 hover:bg-slate-200 dark:bg-slate-900  dark:hover:bg-opacity-80">
+              {`${user?.firstName} ${user?.lastName}`}
+              <ChevronDownIcon
+                className="ml-2 -mr-1 h-5 w-5"
+                aria-hidden="true"
+              />
+            </Menu.Button>
+          </div>
+          <Menu.Items
+            className="absolute right-0 mt-2 w-48 origin-top-right divide-y divide-slate-200 rounded-md bg-white
+           shadow-lg focus:outline-none dark:divide-slate-500 dark:bg-slate-700"
+          >
+            <div className="px-1 py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${active && "bg-slate-300 dark:bg-slate-600"}
+                   group flex w-full items-center rounded-md px-2 py-2 text-slate-900 dark:text-white`}
+                  >
+                    Settings
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${active && "bg-slate-300 dark:bg-slate-600"}
+                 group flex w-full items-center rounded-md px-2 py-2 text-slate-900 dark:text-white`}
+                  >
+                    Get Help
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${active && "bg-slate-300 dark:bg-slate-600"}
+                group flex w-full items-center rounded-md px-2 py-2 text-slate-900 dark:text-white`}
+                    onClick={() => dispatch(setLogout())}
+                  >
+                    Log Out
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Menu>
       </div>
     </div>
   );
