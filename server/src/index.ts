cef166c88435cfda1449535744d6160d07e6e2fd
Jakub Syska <jsyska@it-dev.pl>
2023-04-13 09:31:44 +0200

    Fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,14 @@ const app: Express = express();
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
 
 app.use(express.json());
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+      "img-src": ["'self'", "data:", "https:"],
+    }
+  })
+);
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb" }));
