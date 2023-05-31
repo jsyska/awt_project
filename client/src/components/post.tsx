1e1549f971d237b1ca9f3037c12c09f9c774b913
Jan Krokos <jankrokos@gmail.com>
2023-06-01 01:23:09 +0200

    Fix register and login pages

--- client/src/components/post.tsx
+++ client/src/components/post.tsx
@@ -129,7 +129,7 @@ const PostView = ({
         <div className=" flex flex-1 items-center justify-center gap-2">
           <RocketLaunchIcon
             className={`h-9 w-9 cursor-pointer rounded-md p-2 sm:hover:bg-gray-300 sm:dark:hover:bg-gray-600 
-                        ${isLiked && "text-yellow-400"} 
+                        ${isLiked && "text-indigo-500"} 
                         ${likeEffect && "animate-like"}`}
             onClick={() => {
               patchLike();
